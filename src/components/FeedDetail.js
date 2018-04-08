cd60c9ec6ef2510c08435d5da86786bde93c4f41
Costa Gregorio Alessio <gregorio@imagine-mobile.com>
2018-04-08 16:29:20 +0100

    Feed detail layout and moment

--- src/components/FeedDetail.js
+++ src/components/FeedDetail.js
@@ -1,20 +1,59 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Moment from 'react-moment';
 
 class FeedDetail extends Component {
   render() {
-    const {data} = this.props
+    const { data } = this.props;
+    const tags = data.tags.split(" ");
+    const rawDescription = data.description.match(/<p>.*?<\/p>/g);
+    const description = rawDescription.length >= 3 ? rawDescription[2] : ''
+
     return (
       <article>
-        <h2>{data.title}</h2>
-        <a>Photo author</a> | Published: {data.date_taken}
-        <a onClick={this.props.onClickBack}>Back</a>
-        <div className="col-left">
-            <img alt={data.title} src={data.media.m} />
+        <header>
+          <h2>
+            <a href={data.link} target="_blank">
+              {data.title}
+            </a>
+          </h2>
+          <a className="back-button" onClick={this.props.onClickBack}>
+            Back
+          </a>
+        </header>
+        <div className="article-details">
+          <a
+            href={`https://www.flickr.com/photos/${data.author_id}`}
+            target="_blank"
+          >
+            Photo author
+          </a>{" "}
+          | Published: <Moment format="Do MMM YYYY [at] kk:mm">{data.date_taken}</Moment>
         </div>
-        <div className="col-right">
-          <p>{data.description}</p>
-          Tags:{data.tags}
+        <div className="article-body">
+          <div className="col-left">
+            <img alt={data.title} src={data.media.m} />
+          </div>
+          <div className="col-right">
+            <p dangerouslySetInnerHTML={{ __html: description }} />
+            <div className="article-tags">
+              Tags:
+              <ul>
+                {tags.map(tag => {
+                  return (
+                    <li key={tag}>
+                      <a
+                        href={`https://www.flickr.com/photos/tags/${tag}`}
+                        target="_blank"
+                      >
+                        {tag}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
         </div>
       </article>
     );
--- src/components/FeedListItem.js
+++ src/components/FeedListItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Moment from 'react-moment';
 
 class FeedListItem extends Component {
   render() {
@@ -7,14 +8,29 @@ class FeedListItem extends Component {
     return (
       <article>
         <div className="col-left">
-          <a onClick={this.props.onClickItem}><img alt={data.title} src={data.media.m} /></a>
+          <a onClick={this.props.onClickItem}>
+            <img alt={data.title} src={data.media.m} />
+          </a>
         </div>
         <div className="col-right">
-          <h3><a onClick={this.props.onClickItem}>{data.title}</a></h3>
+          <h3>
+            <a onClick={this.props.onClickItem}>{data.title}</a>
+          </h3>
           <ul>
-            <li className="post-author">Post author</li>
-            <li className="date">Published on {data.date_taken}</li>
-            <li className="view-flicker">View on Flickr</li>
+            <li className="post-author">
+              <a
+                href={`https://www.flickr.com/photos/${data.author_id}`}
+                target="_blank"
+              >
+                Post author
+              </a>
+            </li>
+            <li className="date">Published on <Moment format="Do MMM YYYY [at] kk:mm">{data.date_taken}</Moment></li>
+            <li className="view-flicker">
+              <a href={data.link} target="_blank">
+                View on Flickr
+              </a>
+            </li>
           </ul>
         </div>
       </article>
